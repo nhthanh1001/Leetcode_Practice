ac782c0e0b18730dc58b3213c54c30250e1d561f
Hai Thanh <haithanh.nguyen@estuary.solutions>
2026-03-04 22:59:58 +0700

    Quiz 1

---
+++ Sum_of_unique_elements.ts
@@ -0,0 +1,41 @@
+/*
+You are given an integer array nums. The unique elements of an array are the elements that appear exactly once in the array.
+
+Return the sum of all the unique elements of nums.
+
+Example 1:
+
+Input: nums = [1,2,3,2]
+Output: 4
+Explanation: The unique elements are [1,3], and the sum is 4.
+Example 2:
+
+Input: nums = [1,1,1,1,1]
+Output: 0
+Explanation: There are no unique elements, and the sum is 0.
+Example 3:
+
+Input: nums = [1,2,3,4,5]
+Output: 15
+Explanation: The unique elements are [1,2,3,4,5], and the sum is 15.
+*/
+
+function sumOfUnique(nums: number[]): number {
+  const numsMap = new Map<number, number>();
+
+  // Đếm số lần xuất hiện
+  for (const num of nums) {
+    numsMap.set(num, (numsMap.get(num) ?? 0) + 1);
+  }
+
+  // Tính tổng các phần tử xuất hiện đúng 1 lần
+  let sum = 0;
+
+  for (const [num, count] of numsMap) {
+    if (count === 1) {
+      sum += num;
+    }
+  }
+
+  return sum;
+}
