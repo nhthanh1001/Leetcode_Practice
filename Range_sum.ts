dbd730137ccebe36d52f1747f5385204dfef93e1
Hai Thanh <haithanh.nguyen@estuary.solutions>
2026-03-04 23:28:50 +0700

    Quiz 2

---
+++ Range_sum.ts
@@ -0,0 +1,42 @@
+/*Given an integer array nums, handle multiple queries of the following type:
+
+Calculate the sum of the elements of nums between indices left and right inclusive where left <= right.
+Implement the NumArray class:
+
+NumArray(int[] nums) Initializes the object with the integer array nums.
+int sumRange(int left, int right) Returns the sum of the elements of nums between indices left and right inclusive (i.e. nums[left] + nums[left + 1] + ... + nums[right]).
+ 
+
+Example 1:
+
+Input
+["NumArray", "sumRange", "sumRange", "sumRange"]
+[[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
+Output
+[null, 1, -1, -3]
+
+Explanation
+NumArray numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
+numArray.sumRange(0, 2); // return (-2) + 0 + 3 = 1
+numArray.sumRange(2, 5); // return 3 + (-5) + 2 + (-1) = -1
+numArray.sumRange(0, 5); // return (-2) + 0 + 3 + (-5) + 2 + (-1) = -3 */
+
+class NumArray {
+  private prefixSums: number[];
+  constructor(nums: number[]) {
+    this.prefixSums = new Array(nums.length + 1).fill(0);
+    for (let i = 0; i < nums.length; i++) {
+      this.prefixSums[i + 1] = this.prefixSums[i] + nums[i];
+    }
+  }
+
+  sumRange(left: number, right: number): number {
+    return this.prefixSums[right + 1] - this.prefixSums[left];
+  }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = new NumArray(nums)
+ * var param_1 = obj.sumRange(left,right)
+ */
